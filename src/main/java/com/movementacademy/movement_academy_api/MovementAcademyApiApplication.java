0b7a740917b02fa9e0b0635e9d471fc3e8583fd0
leotakahashii <leonardotakahashii@yahoo.com>
2026-08-24 15:57:22 -0300

    chore: iniciando o projeto com spring boot, JPA e PostgreSQL.

---
+++ src/main/java/com/movementacademy/movement_academy_api/MovementAcademyApiApplication.java
@@ -0,0 +1,13 @@
+package com.movementacademy.movement_academy_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovementAcademyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovementAcademyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movementacademy/movement_academy_api/MovementAcademyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movementacademy.movement_academy_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovementAcademyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
